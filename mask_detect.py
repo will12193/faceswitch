3da6857be0e9ee8ab9b4a89f02b618cdd9fdb49d
Wil <willcumines@gmail.com>
2022-05-09 16:27:01 +1000

    rejig

--- mask_detect.py
+++ mask_detect.py
@@ -14,6 +14,9 @@ from tensorflow.keras.models import load_model
 def detect_mask(image, model, rfModel, verbose=False):
     # Find faces
     theFaces = detect_faces(image, model = rfModel)
+    if isinstance(theFaces, tuple):
+        return image
+    
     imageOutput = copy.deepcopy(image)
 
     for i, f in theFaces.items():
@@ -30,14 +33,14 @@ def detect_mask(image, model, rfModel, verbose=False):
             colour = (255, 0, 255)
         else:
             # Preprocess image for model
-            face = cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
-            face = cv2.resize(face,(96,96))
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (96, 96))
             face = img_to_array(face)
             face = preprocess_input(face)
             face = np.expand_dims(face,axis=0)
             
             # Use model to determine if mask is worn or not
-            (withoutMask, mask) = model.predict(face)[0]
+            (mask, withoutMask) = model.predict(face)[0]
             if verbose:
                 print("MASK: ", mask)
                 print("Without: ", withoutMask)
@@ -191,33 +194,25 @@ def detect_faces(img, threshold=0.9, model = None, allow_upscaling = True):
 
 
 def main():
-    # Load models
     model = load_model(r'./training/models/maskdetection_4000_32_100.h5')
     rfModel = RetinaFace.build_model()
 
-    cap = cv2.VideoCapture(r'./testing/test.mp4')
+    cap = cv2.VideoCapture(0)
     if (cap.isOpened()== False):
-        raise Exception("Error opening video stream or file")
-        
-    plt.ion()
-    ret, frame = cap.read()
-    if ret == True:
-        img = plt.imshow(frame)
+        raise Exception("Error opening VideoCapture")
 
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
             detFrame = detect_mask(frame, model, rfModel)
-            img.set_data(detFrame)
-            plt.pause(0.02)
-
+            cv2.imshow('frame', detFrame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
         else: 
             break
 
-    plt.ioff() 
-    plt.show()
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
