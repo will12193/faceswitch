e7fea3ec748c256ecccc8349fb25b573dbe65b33
Wil <willcumines@gmail.com>
2022-05-14 10:56:29 +1000

    refactor

--- training/trainer.py
+++ training/trainer.py
@@ -23,8 +23,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-# https://github.com/techyhoney/Facemask_Detection
-
+modelName = "maskdetection_4000_32_100"
 dataset = "./dataset"
 imagePaths=list(paths.list_images(dataset))
 
@@ -50,7 +49,8 @@ labels = to_categorical(labels)
 # Split data
 print('Split data')
 
-train_X,test_X,train_Y,test_Y = train_test_split(data,labels,test_size=0.20,random_state=10,stratify=labels)
+train_X, test_X, train_Y, test_Y = train_test_split(
+            data, labels, test_size=0.20, random_state=10, stratify=labels)
 
 aug=ImageDataGenerator(
                        rotation_range=20,
@@ -61,30 +61,35 @@ aug=ImageDataGenerator(
                        fill_mode='nearest'
                        )
 
-input_shape = (96,96,3)
 
-
-# Build Model
+# Build the model
 print('Build Model')
-
+input_shape = (96,96,3)
 input_image = Input(shape=input_shape)
+
 # 1st Conv layer
 model = Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=input_shape)(input_image)
 model = MaxPooling2D((2, 2),padding='same')(model)
+
 # 2nd Conv layer
 model = Conv2D(32, (3, 3), activation='relu', padding='same')(model)
 model = MaxPooling2D((2, 2),padding='same')(model)
+
 # 3rd Conv layer
 model = Conv2D(64, (3, 3), activation='relu', padding='same')(model)
 model = MaxPooling2D((2, 2),padding='same')(model)
+
 # 4th Conv layer
 model = Conv2D(128, (3, 3), activation='relu', padding='same')(model)
 model = MaxPooling2D((2, 2),padding='same')(model)
+
 # 5th Conv layer
 model = Conv2D(256, (3, 3), activation='relu', padding='same')(model)
 model = MaxPooling2D((2, 2),padding='same')(model)
-# FC layers
+
+# Fully connected layers
 model = Flatten()(model)
+
 #model = Dense(1024, kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01))(model)
 model = Dense(1024)(model)
 #model = Dropout(0.2)(model)
@@ -96,17 +101,18 @@ model = Dense(64)(model)
 output= Dense(2, activation='softmax')(model)
 model = Model(inputs=[input_image], outputs=[output])
 
+# Print model structure
 model.summary()
 print(model.summary())
 
+# Define model learning rate, number of epochs, bach size, optimizer, and loss function
 lr=0.0005
 Epochs=100
 BS=32
-
 opt=Adam(learning_rate=lr,decay=lr/Epochs)
-
 model.compile(loss='binary_crossentropy',optimizer=opt,metrics=['accuracy'])
 
+# Fit the model
 history=model.fit(
     aug.flow(train_X,train_Y,batch_size=BS),
     steps_per_epoch=len(train_X)//BS,
@@ -115,7 +121,7 @@ history=model.fit(
     epochs=Epochs
 )
 
-
+# Make training vs validation loss graphs
 loss_train = history.history['loss']
 loss_val = history.history['val_loss']
 epochs = range(1,101)
@@ -126,7 +132,9 @@ plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
 plt.show()
+plt.savefig(modelName+'-TrainingLoss.png')
 
+# Make training vs validation accuracy graphs
 acc_train = history.history['accuracy']
 acc_val = history.history['val_accuracy']
 epochs = range(1,101)
@@ -137,13 +145,19 @@ plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.show()
+plt.savefig(modelName+'-TrainingAcc.png')
 
+# Plot loss and accuracy on the same graph
 pd.DataFrame(history.history).plot(figsize=(8,5))
 plt.show()
+plt.savefig(modelName+'-LossAndAcc.png')
 
-# model.save('maskdetection_4000_32_100.h5')
-model.save(os.path.join('./models', 'maskdetection_4000_32_100.h5'))
+# Save the model 
+model.save(os.path.join('../app/models', modelName+'.h5'))
 
+# Print reports
 predict=model.predict(test_X,batch_size=BS)
 predict=np.argmax(predict,axis=1)
 print(classification_report(test_Y.argmax(axis=1),predict,target_names=lb.classes_))
+
+"#<CITATION: USES SOME CODE FROM https://github.com/techyhoney/Facemask_Detection, Hiten Goyal>"
